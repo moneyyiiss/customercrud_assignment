585a459bc29196bf2004fffa4eab3648475e4021
Manish <moneyyiiss@gmail.com>
2024-06-11 18:44:03 +0530

    tested crud operation by postman

--- src/main/java/com/sunbase/customercrud/controller/CustomerController.java
+++ src/main/java/com/sunbase/customercrud/controller/CustomerController.java
@@ -15,26 +15,35 @@ public class CustomerController {
     private CustomerService customerService;
 
     // create customer
+    // http://localhost:8080/api/customers
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer){
         return customerService.createCustomer(customer);
     }
 
+    // update customer
+    // http://localhost:8080/api/customers/id
     @PutMapping("/{id}")
     public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails){
         return customerService.updateCustomer(id, customerDetails);
     }
 
+    // get all customer
+    // http://localhost:8080/api/customers
     @GetMapping
     public List<Customer> getAllCustomers(){
         return customerService.getAllCustomers();
     }
 
+    // get customer by id
+    // http://localhost:8080/api/customers/id
     @GetMapping("/{id}")
     public Customer getCustomerById(@PathVariable Long id){
         return customerService.getCustomerById(id);
     }
 
+    // delete customer by id
+    // http://localhost:8080/api/customers/id
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCustomer(@PathVariable Long id){
         customerService.deleteCustomer(id);
